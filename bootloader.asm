c3cb5648be2214158af5e55e2d560c8774427023
Pineconium <101128219+Pineconium@users.noreply.github.com>
2023-05-25 18:40:17 +0100

    Add files via upload

---
+++ bootloader.asm
@@ -0,0 +1,20 @@
+[org 0x7c00]
+mov ah, 0x0e
+mov bx, variableName
+
+printString:
+    mov al, [bx]
+    cmp al, 0
+    je end
+    int 0x10
+    inc bx
+    jmp printString
+end:
+
+jmp $
+
+variableName:
+    db "CHOACURY!",
+
+times 510-($-$$) db 0
+db 0x55, 0xaa
