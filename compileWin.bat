42d440113e8ba7c7eb2eecfb0d30631d26c35d7f
Pineconium <101128219+Pineconium@users.noreply.github.com>
2023-08-05 19:32:41 +0100

    Update compileWin.bat

--- compileWin.bat
+++ compileWin.bat
@@ -1,10 +1,13 @@
 @echo off
-title Choacury compiler for WINDOWS
+title Choacury Compiler WINDOWS version
 rem Start making stuff
 echo Compiling file(s)...
-nasm -f bin src/boot/boot.asm -o boot.bin
-nasm -f bin src/kernel/kernel.asm -o kernasm.bin
-copy /b boot.bin + kernasm.bin Choacury.flp
-rem Add some code for when the rest of the code is ready ;-)
+wsl nasm -f elf32 src/kernel/krnentry.asm -o krnent.o
+wsl gcc -m32 -c src/kernel/kernel.c -o krnc.o
+wsl gcc -m32 -c src/drivers/ports.c -o drivport.o
+wsl gcc -m32 -c src/drivers/idt.c -o drividt.o
+wsl gcc -m32 -c src/drivers/utils.c -o driv_utils.o
+wsl gcc -m32 -c src/drivers/vga.c -o driv_vga.o
+wsl ld -m elf_i386 -T src/linker.ld --allow-multiple-definition -o kernel krnent.o krnc.o drivport.o drividt.o driv_utils.o driv_vga.o
 echo Booting into QEMU
-qemu-system-x86_64 -fda Choacury.flp
+qemu-system-x86_64 -kernel kernel
