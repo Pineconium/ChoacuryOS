46b8b7a59987fee7ddd9ec9370ed62c9961ce4ef
Pineconium <101128219+Pineconium@users.noreply.github.com>
2023-08-07 03:23:35 +0100

    Update kernel.c

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -10,7 +10,7 @@ I call it the Choakernel! The code still needs to be finished
 void k_main() 
 {
     k_clear_screen();
-    k_printf("WELCOME TO CHOAKERN. The Choacury Kernel!\nVersion: Internal Pharaoh 1\nCopyright (C) Pineconium 2023-\n\nUNDERCONSTRUCTION", 0);
+    k_printf("WELCOME TO CHOAKERN. The Choacury Kernel!\nVersion: Kernel Test 1\nCopyright (C) Pineconium 2023-\n\nUNDERCONSTRUCTION", 0);
     port_byte_out(0x3d4, 14);
     int position = port_byte_in(0x3d5);
     position = position << 8;
