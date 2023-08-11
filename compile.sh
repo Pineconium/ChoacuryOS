435b36a761448a148bbd34c4e0b777fd493cf736
Pineconium <101128219+Pineconium@users.noreply.github.com>
2023-08-11 18:31:55 +0100

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -9,7 +9,16 @@ gcc -m32 -c src/drivers/ports.c -o drivport.o
 gcc -m32 -c src/drivers/idt.c -o drividt.o
 gcc -m32 -c src/drivers/utils.c -o driv_utils.o
 gcc -m32 -c src/drivers/vga.c -o driv_vga.o
-ld -m elf_i386 -T src/linker.ld --allow-multiple-definition -o kernel krnent.o krnc.o drivport.o drividt.o driv_utils.o driv_vga.o
+ld -m elf_i386 -T src/linker.ld --allow-multiple-definition krnent.o krnc.o drivport.o drividt.o driv_utils.o driv_vga.o -o ChoacuryOS.bin -nostdlib
+# Check the binary file is x86 multiboot file or not
+echo "Checking if BINARY is GRUB-Ready..."
+grub-file --is-x86-multiboot ChoacuryOS.bin
+# Building the ISO file
+echo "Making ISO file"
+mkdir -p isodir/boot/grub
+cp ChoacuryOS.bin isodir/boot/ChoacuryOS.bin
+cp src/boot/grub.cfg isodir/boot/grub/grub.cfg
+grub-mkrescue -o ChoacuryOS.iso isodir
 # Add some code for when the rest of the code is ready ;-)
 echo "Booting into QEMU"
-qemu-system-x86_64 -kernel kernel
+qemu-system-x86_64 -cdrom ChoacuryOS.iso
