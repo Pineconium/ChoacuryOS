3e41a1407fa012f0fac98c5d277cbc834a803493
Bananymous <bananymousosq@gmail.com>
2024-01-02 19:33:21 +0200

    Use better types in utils and implement memory_move

--- src/drivers/utils.h
+++ src/drivers/utils.h
@@ -1,10 +1,8 @@
-#ifndef UTIL_H
-#define UTIL_H
+#pragma once
 
 #include "types.h"
 
-void memory_copy(char *source, char *dest, int nbytes);
+void memory_copy(u8 *dest, const u8 *source, u32 nbytes);
+void memory_move(u8 *dest, const u8 *source, u32 nbytes);
 void memory_set(u8 *dest, u8 val, u32 len);
 void int_to_ascii(int n, char str[]);
-
-#endif
