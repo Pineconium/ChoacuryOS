8a8aa5e5ed9f4ed31c288072d71ea42a7cc5b32a
Pineconium <101128219+Pineconium@users.noreply.github.com>
2024-07-28 17:21:05 +0100

    Added (VERY BAREBONES) Mouse Drivers
    Added a mouse.h file with very very minimal code. Meant to be expanded on later and it doesn't do anything.

---
+++ src/drivers/mouse.h
@@ -0,0 +1,21 @@
+/* actually moose drivers yay */
+
+#pragma once
+
+#include "ports.h"
+#include "types.h"
+
+/* well I just need actual code */
+
+void mouse_init(){
+    return; // <-- Replace
+}
+
+void mouse_wait(){
+    return; // <-- Replace
+}
+
+/* grab the cursors position, meant for a cursor */
+void mouse_pos(){
+    return; // <-- Replace
+}
