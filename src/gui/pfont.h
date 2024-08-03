60a0c1d7c0d5ad49d0c7eb5c3210e42251a58ca7
Pineconium <101128219+Pineconium@users.noreply.github.com>
2024-08-03 21:28:17 +0100

    Working on a GUI mono font
    Soon we can draw text on the GUI

---
+++ src/gui/pfont.h
@@ -0,0 +1,12 @@
+/* Choacurys Standard and Small Monospace font */
+/* made for the GUI */
+
+/* we probs need more includes then this lol */
+#include "bindraw.h"
+
+/* add character maps here
+* We need
+* - All 27 letters of the Latin alphabet (both Upper and lower case)
+* - Numbers 0-9
+* - And some extra symbols like puncs.
+*/
