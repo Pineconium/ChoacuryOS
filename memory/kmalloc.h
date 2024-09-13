6e1533cc5c4025853bcf42d9a7d01194554910f0
Pineconium <101128219+Pineconium@users.noreply.github.com>
2024-09-13 18:31:50 +0100

    Delete memory directory

--- memory/kmalloc.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <stddef.h>
-
-void kmalloc_init();
-void* kmalloc(size_t size);
-void kfree(void* ptr);
--- memory/pmm.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "../drivers/types.h"
-#include "../kernel/multiboot.h"
-
-extern u64 g_total_pmm_bytes;
-
-void pmm_init(const multiboot_info_t* multiboot_info);
