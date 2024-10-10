3bf2f4854df60f8acc20d4df28bee1f54fdc70f8
Pineconium <101128219+Pineconium@users.noreply.github.com>
2024-10-10 02:13:29 +0100

    Minor Patches

--- src/shell/commands/command.c
+++ src/shell/commands/command.c
@@ -47,21 +47,22 @@ void shell_init_commands_list() {
  */
 
 Command shell_commands_list[] = {
-    // Command   Alias(es)      Args        Description                                    Function
-    {"help",     {NULL},        "<str:command name/alias> (args)", "Shows all of the available commands",    shell_help_command},
-    {"beep",     {NULL},        "[int:freq.] [int:duration]", "PC Beeper control.",   shell_beep_command}, // DONE
-    {"calc",     {NULL},        "[int:number1] [str:func] [int:number2]", "Literally a calculator.", shell_calc_command}, // DONE
-    {"cat",      {NULL},        "[str:file]", "Print a file contents.",                      shell_cat_command}, // DONE
-    {"cd",       {NULL},        "[str:directory]", "Changes the current directory.",              shell_cd_command}, // DONE
-    {"compdate", {NULL},        "No args.", "Shows the compilation date.",                 shell_compdate_command}, // DONE
-    {"clear",    {"cls", NULL}, "No args.", "Clears the screen.",                          shell_clear_command},
-    {"echo",     {NULL},        "No args.", "Prints string to the console.",               shell_echo_command}, // DONE
-    {"guiload",  {NULL},        "No args.", "Loads up the GUI (WIP!)",                     shell_guiload_command},
-    {"ls",       {NULL},        "(str:directory)", "List files in a directory.",                  shell_ls_command}, // DONE
-    {"pause",    {NULL},        "(-t int:time) (-k)", "Pauses the terminal until a keyboard input.", shell_pause_command}, // DONE
-    {"pl",       {NULL},        "No args.", "Shows the connected data devices.",           shell_pl_command}, // DONE
-    {"chstat",   {NULL},        "No args.", "Display system information.",                 shell_chstat_command}, // DONE
-    {"vbetest",  {NULL},        "No args.", "Test Bochs VBE extensions",                   shell_vbetest_command}, // DONE
-    {"whereami", {NULL},        "No args.", "Prints the current directory",                shell_whereami_command}  // DONE
-};
+    // Command   Alias(es)      Args        Description                                                         Function:
+    {"help",     {NULL},        "<str:command name/alias> (args)", "Shows all of the available commands",       shell_help_command},
+    {"beep",     {NULL},        "[int:freq.] [int:duration]", "PC Beeper control.",                             shell_beep_command},  
+    {"calc",     {NULL},        "[int:number1] [str:func] [int:number2]", "Literally a calculator.",            shell_calc_command},  
+    {"cat",      {NULL},        "[str:file]", "Print a file contents.",                                         shell_cat_command},  
+    {"cd",       {NULL},        "[str:directory]", "Changes the current directory.",                            shell_cd_command},  
+    {"compdate", {NULL},        "No args.", "Shows the compilation date.",                                      shell_compdate_command},  
+    {"clear",    {"cls", NULL}, "No args.", "Clears the screen.",                                               shell_clear_command},
+    {"echo",     {NULL},        "No args.", "Prints string to the console.",                                    shell_echo_command},  
+    {"guiload",  {NULL},        "No args.", "Loads up the GUI (WIP!)",                                          shell_guiload_command},
+    {"ls",       {NULL},        "(str:directory)", "List files in a directory.",                                shell_ls_command},  
+    // {"mkdir",    {NULL},        "(str:directory)", "Creates a empty directory.",                                shell_mkdir_command}, // <-- TODO
+    {"pause",    {NULL},        "(-t int:time) (-k)", "Pauses the terminal until a keyboard input.",            shell_pause_command},  
+    {"pl",       {NULL},        "No args.", "Shows the connected data devices.",                                shell_pl_command},  
+    {"chstat",   {NULL},        "No args.", "Display system information.",                                      shell_chstat_command},  
+    {"vbetest",  {NULL},        "No args.", "Test Bochs VBE extensions",                                        shell_vbetest_command},  
+    {"whereami", {NULL},        "No args.", "Prints the current directory",                                     shell_whereami_command} 
+}
 int shell_commands_count = sizeof(shell_commands_list) / sizeof(Command);
