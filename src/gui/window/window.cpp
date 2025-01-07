1dd5b281711a73983cd3a94d919662e4a6bf9c19
William <william@wtdawson.info>
2025-01-07 21:38:30 +0000

    Current progress (doesn't compile because of `Window::resize`)

--- src/gui/window/window.cpp
+++ src/gui/window/window.cpp
@@ -8,46 +8,71 @@ extern "C" {
 }
 #include "window.hpp"
 
-void Window::change_state(WindowState state) {
+inline void* operator new(size_t size) { return kmalloc(size); }
 
+void Window::change_state(WindowState state) {
+    this->state = state;
 }
 
 void Window::change_style(WindowStyle style) {
-
+    this->style = style;
 }
 
 bool Window::move(int64_t x, int64_t y) {
-
+    // De-render, move, render
+    this->derender();
+    this->x = x;
+    this->y = y;
+    this->render();
 }
 
 bool Window::resize(int64_t width, int64_t height) {
-
+    // (If new size smaller) De-render, reallocate buffer, render
+    if(this->width > width || this->height > height) this->derender();
+    buffer = new uint32_t[width * height];
+    render();
+    return true;
 }
 
 bool Window::render() {
-
+    if(this->buffer == nullptr) return false;
+    for(int64_t y = 0; y < this->height; y++) {
+        for(int64_t x = 0; x < this->width; x++) {
+            vbe_putpixel(this->x + x, this->y + y, this->buffer[y * this->width + x]);
+        }
+    }
+    return true;
 }
 
 bool Window::derender() {
-
+    // Replace everything with the default background colour
+    // This needs to be done in future by figuring out if
+    // anything needs to be drawn that is below it.
+    vbe_fillrect(this->x, this->y, this->width, this->height, 0x000000ff);
+    return true;
 }
 
 bool Window::begin_drag() {
-
+    if(drag_offset_x == -1 || drag_offset_y == -1) {
+        return false;
+    }
+    this->is_dragging = true;
+    return true;
 }
 
 bool Window::end_drag() {
-
+    this->is_dragging = false;
+    return true;
 }
 
 void Window::handle_mouse_move(int32_t x, int32_t y) {
-
+    
 }
 
 void Window::handle_mouse_press() {
 
 }
 
-void handle_key_pressed(char key) {
-    
+void Window::handle_key_pressed(char key) {
+
 }
--- src/gui/window/window.hpp
+++ src/gui/window/window.hpp
@@ -29,6 +29,9 @@ private:
     WindowState state;
     char** title;
     bool is_dragging;
+    int32_t drag_offset_x = -1;
+    int32_t drag_offset_y = -1;
+    uint32_t* buffer = nullptr;
 public:
     /// @brief Window initializer
     /// @param _x Window position (X)
@@ -47,20 +50,53 @@ public:
         height = _height;
         style = _style;
         state = _state;
+
+        buffer = new uint32_t[_width * _height];
     }
+    /// @brief Changes the window state
+    /// @param state The new window state
     void change_state(WindowState state);
+    /// @brief Changes the window style
+    /// @param style The new window style
     void change_style(WindowStyle style);
+    /// @brief Moves the window to the new coordinates
+    /// @param x The new X
+    /// @param y The new Y
+    /// @return If it was successful or not
     bool move(int64_t x, int64_t y);
+    /// @brief Resizes the window
+    /// @param width The new width
+    /// @param height The new height
+    /// @return Ifi t was successful or not
     bool resize(int64_t width, int64_t height);
+    /// @brief Renders the window on the screen
+    /// @return If it was successful or not
     bool render();
+    /// @brief De-renders (Removes) the window from the screen
+    /// @return If it was successful or not
+    /// @note This only sets every pixel to 0x000000ff, needs to be done properly in future
     bool derender();
     // Dragging functionality
+    /// @brief Sets the drag offset
+    /// @param x The X drag offset
+    /// @param y The Y drag offset
+    void set_drag_offset(int32_t x, int32_t y);
+    /// @brief Begins dragging, use set_drag_offset before or this will fail
+    /// @returns If it was successful or not
     bool begin_drag();
+    /// @brief Ends dragging
+    /// @return If it was successful or not
     bool end_drag();
     // Mouse functionality
+    /// @brief Handles a mouse move event
+    /// @param x The X coordinate of the mouse
+    /// @param y The Y coordinate of the mouse
     void handle_mouse_move(int32_t x, int32_t y);
+    /// @brief Handles a mouse press event
     void handle_mouse_press();
     // Keyboard functionality (Not implemented immidiately)
+    /// @brief Handles a keyboard key event
+    /// @param key The key that was pressed
     void handle_key_pressed(char key);
 };
 
