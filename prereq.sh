2361cdbd87347e87e8aebb5d2a6cea6b2a2e2b7c
Pineconium <101128219+Pineconium@users.noreply.github.com>
2025-04-18 22:05:42 +0100

    Made Prerequisites Installer Script
    Currently only for Ubuntu and Debian based distributions.

---
+++ prereq.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# NOTICE: Only for Debian/Ubuntu-based distros. Arch, Gentoo, and Fedora will come soon!
+echo "Installing needed software, prepare for a root password prompt"
+sudo apt install gcc makefile grub-common
+echo "Done. Check for any errors and warnings that may come up."
